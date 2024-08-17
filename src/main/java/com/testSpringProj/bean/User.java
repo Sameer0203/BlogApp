e33e797b8758fbc67ea422e79855356c0f57a609
sameer <sameeraroradiamond@gmail.com>
2024-08-17 18:21:40 +0530

    changes

--- src/main/java/com/testSpringProj/bean/User.java
+++ src/main/java/com/testSpringProj/bean/User.java
@@ -56,7 +56,7 @@ public class User implements UserDetails {
 	
 	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	@JoinTable(name = "user_role", 
-	joinColumns = @JoinColumn(name = "user", referencedColumnName = "id"),
+	joinColumns = @JoinColumn(name = "\"user\"", referencedColumnName = "id"),
 	inverseJoinColumns = @JoinColumn(name = "role", referencedColumnName = "id")
 			)
 	private Set<Role> roles = new HashSet<>();
